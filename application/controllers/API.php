16d6be596db388b358303304e5423d3fb4eccd7e
Mukund Salia <mukund.salia@oracle.com>
2015-10-30 17:13:40 -0400

    Making API Page nicer

--- application/controllers/API.php
+++ application/controllers/API.php
@@ -3,9 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class API extends CI_Controller {
 
-    public function index()
-    {
-        $this->load->view('api_welcome');
+    private $subview = null;
+
+    public function index() {
+        $this->setIndexSubview();
+        $this->loadView();
+    }
+
+    private function getData() {
+        $data = array();
+
+        if ($this->subview) {
+            $data["subview"] = $this->subview;
+        }
+
+        return $data;
+    }
+
+    private function loadView() {
+        $this->load->view('api_page', $this->getData());
+    }
+
+    private function setIndexSubview() {
+        $this->subview = "index/landing.php";
     }
 
 }
---
+++ application/views/index/landing.php
@@ -0,0 +1,22 @@
+<div class="section-block">
+    <div class="section">
+        <h3>INTRODUCTION</h3>
+        <p>
+            This API was developed with the goal to have a central repository of data for Pramukh Cup. This data includes player profiles, player stats, team profiles &amp; match records.
+            This API will solely be used to track tournament data and provide the required data for Cricboard.
+        </p>
+    </div>
+    <div class="section">
+        <h3>USAGE</h3>
+        <p>
+            Chitragupta has two primary functions. Import &amp; Retrieve. API will import the Cricboard export data and update user profiles.
+            It will also allow Cricboard to retrieve any data that it needs.
+        </p>
+    </div>
+    <div class="section">
+        <h3>MAINTAINERS</h3>
+        <p>
+            Chitragupta API is mainted by Ankur Patel, Hemal Patel &amp; Mukund Salia. Please contact these individuals before making any major changes to the API.
+        </p>
+    </div>
+</div>
